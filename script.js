dc6c44708fa177517c9b7647bca166963f916ed9
djjean <djjean90@gmail.com>
2021-10-21 22:34:31 +0300

    final

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //Script.js
+
+// const btnOpen = document.querySelectorAll(".fa-chevron-down");
+// const divs = document.querySelectorAll(".faq");
+// const btnCLose = document.querySelectorAll(".fa-times");
+
+// btnOpen.forEach((btn, i) =>
+//   btn.addEventListener("click", function () {
+//     divs.forEach((div) => div.classList.remove("active"));
+//     divs[i].classList.toggle("active");
+//     // divs.forEach((div) => console.log(div[0]));
+//   })
+// );
+
+// btnCLose.forEach((btn, i) =>
+//   btn.addEventListener("click", function () {
+//     divs.forEach((div) => div.classList.remove("active"));
+//   })
+// );
+
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.closest(".faq").classList.toggle("active");
+  });
+});
